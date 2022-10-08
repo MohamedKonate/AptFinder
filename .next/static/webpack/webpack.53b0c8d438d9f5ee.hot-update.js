9a1c746cc9685cc25b250d9be16a8683024688b0
mohamedkonate <mohamed.konate@Live.fr>
2022-10-08 20:41:38 +0200

    Add Travis CI

--- .next/static/webpack/webpack.53b0c8d438d9f5ee.hot-update.js
+++ .next/static/webpack/webpack.53b0c8d438d9f5ee.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "0251a64ea7d9e7da"; }
+/******/ 	__webpack_require__.h = function() { return "ec292c9a61e12329"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
